78c7635aca69a9d5ede1ef1b014c74de719a0b3d
ykatsuro <yu_katsuro@hotmail.com>
2017-06-14 14:38:56 +0000

    メッセージ

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,8 @@
 class Message < ApplicationRecord
+  validates :content, presence: true, length: {maximum: 255}
+  validates :title, presence: true, length: { maximum: 255}
+end
+
+def message_params
+  params.require(:message).permit(:content, :title)
 end
